2cfbfd0d8496396efde9eb8e1a4e2cf24c0c3fd3
Igor Farafonov <entertoinfo29@gmail.com>
2022-04-06 22:15:39 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const {src, dest, parallel, watch} = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require ('gulp-concat');
+const sass        = require('gulp-sass')(require('sass'));
+const concat      = require ('gulp-concat');
 const browserSync = require('browser-sync').create();
+const uglify      = require('gulp-uglify-es').default;
 
 function browsersync(){
     browserSync.init({
@@ -11,21 +12,34 @@ function browsersync(){
     });
 }
 
-function styles() {
-    return src('app/scss/**/**.scss')
+function scripts(){
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/js/main.js'
+])
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles(){
+    return src('app/scss/style.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(concat('style.min.css'))
-        .pipe(dest('./css'))
+        .pipe(dest('app/css'))
         .pipe(browserSync.stream())
 }
 
 function watching(){
     watch(['app/scss/**/*.scss'], styles) 
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts) 
     watch(['app/*.html']).on('change', browserSync.reload)
 }
 
 exports.styles = styles;
+exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 
-exports.default = parallel('watching', 'browsersync');
+exports.default = parallel(scripts, browsersync, watching);
