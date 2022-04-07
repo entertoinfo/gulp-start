db6b24c68df23a386d323f4c559250c5c66d5566
Igor Farafonov <entertoinfo29@gmail.com>
2022-04-07 23:00:19 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const {src, dest, parallel, watch} = require('gulp');
-const sass        = require('gulp-sass')(require('sass'));
-const concat      = require ('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify      = require('gulp-uglify-es').default;
+const sass         = require('gulp-sass')(require('sass'));
+const concat       = require ('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
 
 function browsersync(){
     browserSync.init({
@@ -10,6 +12,24 @@ function browsersync(){
             baseDir: "app/"
         }
     });
+}
+
+function images(){
+    return src('app/images**/*')
+    .pipe(imagemin(
+        [
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]
+        ))
+    .pipe(dest('dist/images'))
 }
 
 function scripts(){
@@ -27,8 +47,22 @@ function styles(){
     return src('app/scss/style.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
+}
+
+function build (){
+    return src ([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base:'app'})
+    .pipe(dest('dist'))
 }
 
 function watching(){
@@ -37,9 +71,11 @@ function watching(){
     watch(['app/*.html']).on('change', browserSync.reload)
 }
 
+exports.build = build;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.images = images;
 
 exports.default = parallel(scripts, browsersync, watching);
